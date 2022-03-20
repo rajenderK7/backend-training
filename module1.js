c9714b780356386aa28ba817a10b3bc800921262
Rajender Katkuri <rajenderkatkuri123@gmail.com>
2022-03-20 23:54:50 +0530

    Implement custom module

---
+++ module1.js
@@ -0,0 +1,17 @@
+const greet = (name) => console.log(`Hello! ${name}.`);
+
+const rajender = "batman";
+
+const student = {
+  name: "Rajender",
+  roll: 25,
+  branch: "AIML",
+};
+
+const nums = [12, 45, 345, 532];
+
+// module.exports = student;
+
+// module.exports = [greet, rajender];
+
+module.exports = { greet, rajender, student, nums };
---
+++ module2.js
@@ -0,0 +1,6 @@
+// const s = require("./module1");
+
+const mod1 = require("./module1");
+
+// console.log(mod1);
+console.log(mod1.student);
